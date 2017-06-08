6e91bdd0c35c6164c94c7035466c8f7cf43ae5c8
icyleaf <icyleaf.cn@gmail.com>
2017-06-08 18:17:28 +0800

    迁移 vim

--- brew/install.sh
+++ brew/install.sh
@@ -2,11 +2,11 @@
 #
 # 初始化 Homebrew
 
+brew_path=`pwd`
 if test ! $(which brew); then
-  echo "  Installing Homebrew for you."
-
   # # 检查并安装 Homebrew
   if test "$(uname)" = "Darwin"; then
+    echo " > Installing Homebrew for you."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
     # 替换为国内源
@@ -15,6 +15,13 @@ if test ! $(which brew); then
 
     cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
     git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+
+    cd $brew_path
+    brew update
+
+    # 安装 Brewfile 里面的内容
+    echo "› Install  bundle"
+    brew bundle -v
   elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
     # 安装 Linuxbrew
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
--- zsh/install.sh
+++ zsh/install.sh
@@ -3,28 +3,24 @@
 # Oh-My-Zsh
 source functions/_bash
 
-if ! [ "$(echo $SHELL)" == '/bin/zsh' ]
-then
+if ! [ "$(echo $SHELL)" == '/bin/zsh' ]; then
   info 'changing to zsh shell. please input [sudo]'
   sudo chsh -s /bin/zsh
   success 'zsh installed'
 fi
 
-if ! [ -d ~/.oh-my-zsh ]
-then
+if ! [ -d ~/.oh-my-zsh ]; then
   info 'git clone oh-my-zsh'
   git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
   success 'oh-my-zsh installed'
 fi
 
-if ! [ -f ~/.oh-my-zsh/themes/icyleaf.zsh-theme ]
-then
-  info 'download icyleaf.zsh-theme'
-  curl -L https://gist.github.com/icyleaf/1016181/raw/d16f05c900e33178a03693c1454c3b5e64bf3b80/icyleaf.zsh-theme -o ~/.oh-my-zsh/themes/icyleaf.zsh-theme
-  success 'icyleaf.zsh-theme downloaded'
+if ! [ -f ~/.oh-my-zsh/themes/icyleaf.zsh-theme ]; then
+  zsh_theme="bullet-train.zsh-theme"
+  info 'download ${zsh_theme}'
+  curl -L http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/${zsh_theme} -o ~/.oh-my-zsh/themes/${zsh_theme}
+  success '${zsh_theme} downloaded'
 fi
-
-success 'zsh installed'
 
 tips '=========Oh-My-Zsh========='
 tips 'please include zsh config: '
