d3eb80dfbea09950a2ce156447e2227236ef957b
icyleaf <icyleaf.cn@gmail.com>
2017-06-08 17:50:42 +0800

    使用 Brewfile

--- brew/install.sh
+++ brew/install.sh
@@ -1,73 +1,22 @@
 #!/usr/bin/env bash
 #
-# Install apps using brew
-source functions/_bash
+# 初始化 Homebrew
 
+if test ! $(which brew); then
+  echo "  Installing Homebrew for you."
 
-brew_install() {
-  RESULT=$(brew ls --versions ${1})
-  NAME=$(echo ${RESULT} | awk '{print $1}')
-  VERSION=$(echo ${RESULT} | awk '{print $2}')
+  # # 检查并安装 Homebrew
+  if test "$(uname)" = "Darwin"; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-  if test ! ${NAME}
-  then
-    info "Installing ${1}"
-    brew install ${1}
-    success "Installed ${1}"
-  else
-    success "${1} already installed: ${VERSION}"
+    # 替换为国内源
+    cd "$(brew --repo)"
+    git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+
+    cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+    git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"; then
+    # 安装 Linuxbrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
   fi
-}
-
-# DevOps
-brew_install ansible
-
-# Git
-brew_install git
-brew_install git-flow-avh
-# brew_install tig
-
-# Development Tools
-brew_install mysql
-brew_install mycli
-brew_install redis
-brew_install node
-brew_install imagemagick
-
-# Development Language
-brew_install crystal-lang
-
-# Tweaks
-brew_install fasd
-brew_install axel
-brew_install mtr
-brew_install tree
-brew_install watch
-brew_install wget
-brew_install ssh-copy-id
-brew_install youtube-dl
-brew_install httpie
-brew_install mobile-shell
-brew_install mitmproxy
-brew_install hugo
-
-# Tmux
-brew_install tmux
-brew_install reattach-to-user-namespace
-
-# macOS Apps
-brew cask install visual-studio-code
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install the-unarchiver
-brew cask install dropbox
-brew cask install android-sdk
-brew cask install virtualbox
-# brew cask install android-studio
-
-# macOS QuickLook Extensions
-brew cask isntall provisionql
-brew cask isntall qlcolorcode
-brew cask isntall qlimagesize
-brew cask isntall quicklook-json
-brew cask isntall quicklookapk
+fi
