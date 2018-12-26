7f35ad5573b2b3aef0f68e38577876af858fdc01
icyleaf <icyleaf.cn@gmail.com>
2018-12-26 18:13:04 +0800

    use spacevim install vim vundle

--- vim/install.sh
+++ vim/install.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 #
-# Vim
+# SpaceVim
 source functions/_bash
 
+# Install SpaceVim
+curl -sLf https://spacevim.org/install.sh | bash
+
 # Create bundle dir
-mkdir -p ~/.vim/bundle
-
-# Install vundle
-if ! [ -d ~/.vim/bundle/vundle ]
-then
-  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-fi
-
-# Install vim plugins from .vimrc
-vim +BundleInstall +BundleClean +qall
-
-success "vim/vundle/bundles installed"
+#mkdir -p ~/.vim/bundle
+#
+## Install vundle
+#if ! [ -d ~/.vim/bundle/vundle ]
+#then
+#  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+#fi
+#
+## Install vim plugins from .vimrc
+#vim +BundleInstall +BundleClean +qall
+#
+#success "vim/vundle/bundles installed"
