8600a6bba092354e500d13c0c1a9b52aa8e17b88
icyleaf <icyleaf.cn@gmail.com>
2019-07-16 19:07:08 +0800

    fix: 修复安装 tmux tpm 的判断

--- tmux/install.sh
+++ tmux/install.sh
@@ -6,9 +6,9 @@ source functions/_bash
 tpm_path="$HOME/.tmux/plugins/tpm"
 tpm_url="https://github.com/tmux-plugins/tpm"
 
-if [ -z "$tpm_path" ]
+if ! [ -d "$tpm_path" ]
 then
     info " > Installing tpm"
-    git clone $tpm_url
+    git clone $tpm_url $HOME/.tmux/plugins/tpm
     success "tpm"
 fi
