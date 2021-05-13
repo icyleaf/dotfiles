f4763924aedd0d450a0d6ac94b25b85c1b0bab1f
icyleaf <icyleaf.cn@gmail.com>
2021-05-13 11:58:09 +0800

    replace Dracula xcode theme source

--- xcode/install.sh
+++ xcode/install.sh
@@ -5,21 +5,14 @@ source functions/_bash
 
 xcode_theme_name="Dracula.xccolortheme"
 xcode_theme_path="$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes/${xcode_theme_name}"
-xcode_theme_url="https://github.com/kreeger/base16-xcode"
 root_path=`pwd`
 
-info "Installing Xcode Theme `pwd`"
-if ! [ -f "$xcode_theme_path" ]
-then
-    if [ -z "base16-xcode" ]
-    then
-      git clone $xcode_theme_url themes
-      mv themes xcode
-    fi
+info " > Installing Xcode Theme Dracula `pwd`"
+if ! [ -f "$xcode_theme_path" ]; then
+  curl -sLf https://raw.githubusercontent.com/dracula/xcode/master/Dracula.xccolortheme -o $xcode_theme_path
 
-    link_file "${root_path}/xcode/themes/${xcode_theme_name}" $xcode_theme_path
-    cd $root_path
-    success "Xcode Theme"
+  cd $root_path
+  success "Xcode Theme Dracula"
 else
-    success "skipped, Xcode Theme was installed `pwd`"
+  success "skipped, Xcode Theme was installed `pwd`"
 fi
