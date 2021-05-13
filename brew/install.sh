33a5f662b4a7e6c4b72c524c53e63e75a50b0bc7
icyleaf <icyleaf.cn@gmail.com>
2021-05-13 12:00:50 +0800

    refactor: 设置更换源的方法

--- brew/install.sh
+++ brew/install.sh
@@ -8,32 +8,31 @@ brew_path=`pwd`
 
 export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
 export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
-export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+# export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
 
-info "Installing homebrew"
+info " > Installing homebrew"
 if test ! $(which brew); then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    # 替换为国内源
-    git -C "$(brew --repo)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
-    BREW_TAPS="$(brew tap)"
-    for tap in core cask{,-fonts,-drivers,-versions}; do
-        if echo "$BREW_TAPS" | grep -qE "^homebrew/${tap}\$"; then
-            # 将已有 tap 的上游设置为本镜像并设置 auto update
-            # 注：原 auto update 只针对托管在 GitHub 上的上游有效
-            git -C "$(brew --repo homebrew/${tap})" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-${tap}.git
-            git -C "$(brew --repo homebrew/${tap})" config homebrew.forceautoupdate true
-        else   # 在 tap 缺失时自动安装（如不需要请删除此行和下面一行）
-            brew tap --force-auto-update homebrew/${tap} https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-${tap}.git
-        fi
-    done
+  # 安装 tap
+  brew tap homebrew/cask-fonts
 
-    cd $brew_path
-    brew update-reset
+  # 替换为国内源
+  git -C "$(brew --repo)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+  BREW_TAPS="$(brew tap)"
+  for tap in core services cask{,-fonts}; do
+    if echo "$BREW_TAPS" | grep -qE "^homebrew/${tap}\$"; then
+      git -C "$(brew --repo homebrew/${tap})" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-${tap}.git
+      git -C "$(brew --repo homebrew/${tap})" config homebrew.forceautoupdate true
+    fi
+  done
 
-    success "homebrew"
+  cd $brew_path
+  brew update-reset
+
+  success "homebrew"
 else
-    success "skipped, homebrew was installed `pwd`"
+  success "skipped, homebrew was installed `pwd`"
 fi
 
 # 安装 Brewfile 里面的内容
