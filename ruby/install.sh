b2bd30e01d62ab88b9a4243f6e5ffe08de17d508
icyleaf <icyleaf.cn@gmail.com>
2021-05-13 11:59:58 +0800

    refactor: ruby 和 rvm 配置合二为一

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,8 +1,38 @@
 #!/usr/bin/env bash
 #
 # Ruby 依赖文件
-
 source functions/_bash
+
+rvm_db_path="$HOME/.rvm/user/db"
+rvm_ruby_url="ruby_url=https://cache.ruby-china.com/pub/ruby"
+
+ruby_version=2.7
+
+info " > Installing rvm"
+if test ! $(which rvm); then
+  gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  curl -sSL https://get.rvm.io | bash -s stable
+  source ~/.bashrc
+  source ~/.bash_profile
+  success "rvm"
+else
+    success "skipped, rvm was installed `pwd`"
+fi
+
+info " > Replace RVM source to ruby-china"
+if [ -z "$rvm_db_path" ]; then
+  if ! grep -q "$rvm_ruby_url" $rvm_db_path; then
+    echo "ruby_url=$rvm_ruby_url" > ~/.rvm/user/db
+    success 'rvm source replaced'
+  fi
+else
+    success "skipped, rvm was replaced `pwd`"
+fi
+
+info " > Install Ruby $ruby_version"
+rvm install 2.7 --disable-binary
+rvm use 2.7 --default
+success "Ruby $ruby_version"
 
 info " > Installing ruby gems"
 for gem in "irbtools" \
@@ -20,4 +50,4 @@ for gem in "irbtools" \
     gem install "${gem}"
   fi
 done
-success "ruby"
+success "ruby gems"
