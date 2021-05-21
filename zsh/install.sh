202d2a2d6790d720437000db5beb0e16aced56d9
icyleaf <icyleaf.cn@gmail.com>
2021-05-21 16:04:32 +0800

    feat: zsh 支持 p10k 主题

--- zsh/install.sh
+++ zsh/install.sh
@@ -22,16 +22,26 @@ if ! [ -d $HOME/.oh-my-zsh ]; then
   tips '==========================='
 fi
 
+omz_custom_path=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+root_path=`pwd`
+
 zsh_theme="bullet-train.zsh-theme"
-if ! [ -f $HOME/.oh-my-zsh/themes/$zsh_theme ]; then
+if ! [ -f $omz_custom_path/themes/$zsh_theme ]; then
   info "download ${zsh_theme}"
-  curl -sLf http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/${zsh_theme} -o ~/.oh-my-zsh/themes/${zsh_theme}
+  curl -sLf http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/${zsh_theme} -o "$omz_custom_path/themes/${zsh_theme}"
   success "${zsh_theme} downloaded"
 fi
 
 zsh-autosuggestions="zsh-autosuggestions"
-if ! [ -f ~/.oh-my-zsh/custom/plugins/$autosuggestions ]; then
+if ! [ -f $omz_custom_path/plugins/$autosuggestions ]; then
   info "download ${autosuggestions}"
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$omz_custom_path/plugins/${autosuggestions}"
   success "${autosuggestions} downloaded"
+fi
+
+powerlevel10="powerlevel10"
+if ! [ -f $omz_custom_path/themes/$powerlevel10 ]; then
+  info "download ${powerlevel10}"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$omz_custom_path/themes/${powerlevel10}"
+  success "${powerlevel10} downloaded"
 fi
