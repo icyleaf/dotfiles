8135639634a6215047c67f7840f8f67a3fe18141
icyleaf <icyleaf.cn@gmail.com>
2021-07-16 16:02:18 +0800

    fix: 修复 xcode 安装不了主题

--- xcode/install.sh
+++ xcode/install.sh
@@ -9,15 +9,16 @@ fi
 source functions/_bash
 
 xcode_theme_name="Dracula.xccolortheme"
-xcode_theme_path="$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes/${xcode_theme_name}"
+xcode_theme_path="$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes"
 root_path=`pwd`
 
 info " > Installing Xcode Theme Dracula `pwd`"
 if ! [ -f "$xcode_theme_path" ]; then
-  curl -sLf https://raw.githubusercontent.com/dracula/xcode/master/Dracula.xccolortheme -o $xcode_theme_path
+  mkdir -p $xcode_theme_path
+  curl -sLf https://raw.githubusercontent.com/dracula/xcode/master/Dracula.xccolortheme -o "${xcode_theme_path}/${xcode_theme_name}"
 
   cd $root_path
-  success "Xcode Theme Dracula"
+  success "Xcode Theme Dracula (Xcode restart required)"
 else
   success "skipped, Xcode Theme was installed `pwd`"
 fi
