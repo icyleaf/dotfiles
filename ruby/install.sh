7ebfe4b8281bb9380a9b644793dd299f6ad7d06d
icyleaf <icyleaf.cn@gmail.com>
2021-07-18 20:22:38 +0800

    fix: import rvm gpg keys

--- ruby/install.sh
+++ ruby/install.sh
@@ -9,6 +9,7 @@ ruby_version=2.7.2
 
 info "Installing rvm"
 if test ! $(which rvm); then
+  gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   curl -sSL https://get.rvm.io | bash -s stable
   source ~/.zshrc
   success "rvm"
