2d5bdbac2d057238b83b913542a637d8c55fbc60
icyleaf <icyleaf.cn@gmail.com>
2022-02-17 18:08:19 +0800

    feat: 增加 android sdk path

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -119,3 +119,8 @@ export HOMEBREW_NO_ANALYTICS=1
 
 # gpg
 export GPG_TTY=$(tty)
+
+# Android SDK 路径（Android Studio）
+export REPO_OS_OVERRIDE=macosx
+export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
+export PATH="${ANDROID_SDK_ROOT}/platform-tools:${ANDROID_SDK_ROOT}/tools/bin:$PATH"
