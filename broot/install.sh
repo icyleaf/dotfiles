70291be4b6e1895ca96cf7640aa7b5b979e653c5
icyleaf <icyleaf.cn@gmail.com>
2022-06-14 13:33:10 +0800

    fix: link broot

--- broot/install.sh
+++ broot/install.sh
@@ -4,17 +4,13 @@
 
 source functions/_lib.sh
 
+BROOT_PATH=$(which broot)
+
 info "Installing br"
-if test ! $(which broot); then
+if test ! "$BROOT_PATH"; then
   fail "broot was not installed, run brew/install.sh to install first."
 fi
 
-if [ -z "$(ls /usr/local/bin | grep -i broot)" ]; then
-  ln -s /usr/local/Cellar/broot/0.1.0/bin/broot /usr/local/bin/broot
-else
-  echo "broot was installed"
-fi
-
 # https://dystroy.org/broot/install-br/
-broot --install
+$BROOT_PATH --install
 success "br bundle installed"
