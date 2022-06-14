3d21f9e793a95224d6adcc5692cb1f5e7a591c50
icyleaf <icyleaf.cn@gmail.com>
2022-06-14 15:29:57 +0800

    feat: add linearMouse

---
+++ linnearmouse/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# 复制 linnearMouse 的配置文件
+
+source functions/_lib.sh
+ensure_macos
+
+linearmouse_name="com.lujjjh.LinearMouse.plist"
+linearmouse_dst="${HOME:-~}/Library/Preferences/${linearmouse_name}"
+
+link_file "${DIRPATH}/Preferences/${linearmouse_name}" "$linearmouse_dst"
