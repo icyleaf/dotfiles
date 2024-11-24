5059e2f1b78622ac228a78cd71fa16b9f13d1bba
icyleaf <icyleaf.cn@gmail.com>
2024-11-24 19:00:45 +0800

    feat(linux): change wezterm font size

--- config/general/wezterm/config/fonts.lua
+++ config/general/wezterm/config/fonts.lua
@@ -3,7 +3,7 @@ local platform = require('utils.platform')
 
 -- local font = 'Maple Mono SC NF'
 local font_family = 'MesloLGS Nerd Font Mono'
-local font_size = platform.is_mac and 14 or 9
+local font_size = platform.is_mac and 14 or 12
 
 return {
   font = wezterm.font({
