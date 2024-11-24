a2e365264b3186fa2f8e0c3e3d05ac15d1228911
icyleaf <icyleaf.cn@gmail.com>
2024-11-24 21:54:19 +0800

    fix(linux): gnupg configure

--- config/general/gnupg/install.sh
+++ config/general/gnupg/install.sh
@@ -9,7 +9,8 @@ gnupg_dst="${HOME:-~}/.gnupg"
 gnupg_agent_config_path="${gnupg_dst}/${gnupg_name}"
 
 install_pinentry_mac () {
-  if test ! $(which pinentry-mac); then
+  local pinentry_path=$(command -v pinentry-mac)
+  if test ! "$pinentry_path"; then
     if test ! $(which brew); then
       fail "brew was not installed, run brew/install.sh to install first."
     fi
@@ -17,11 +18,12 @@ install_pinentry_mac () {
     brew install gnupg pinentry-mac
   fi
 
-  configure_gpg_agent_file $(which pinentry-mac)
+  configure_gpg_agent_file "$pinentry_path"
 }
 
 install_pinentry_touchid () {
-  if test ! $(which pinentry-touchid); then
+  local pinentry_path=$(command -v pinentry-touchid)
+  if test ! "$pinentry_path"; then
     if test ! $(which brew); then
       fail "brew was not installed, run brew/install.sh to install first."
     fi
@@ -32,29 +34,30 @@ install_pinentry_touchid () {
   echo "Disabled gpg store passphrase in keychain"
   defaults write org.gpgtools.common DisableKeychain -bool yes
 
-  configure_gpg_agent_file $(which pinentry-touchid)
+  configure_gpg_agent_file $pinentry_path
 }
 
 install_pinentry_tty () {
-  configure_gpg_agent_file $(which pinentry-touchid)
+  configure_gpg_agent_file $(command -v pinentry-tty)
 }
 
-install_pinentry_cursues () {
-  configure_gpg_agent_file $(which pinentry-cursues)
+install_pinentry_curses () {
+  configure_gpg_agent_file $(command -v pinentry-curses)
 }
 
 install_wayprompt () {
-  if test ! $(which wayprompt); then
+  local pinentry_path=$(command -v wayprompt)
+  if test ! "$wpath"; then
     if test $(which paru); then
       paru -S wayprompt
-    else if test $(which yay); then
+    elif test $(which yay); then
       yay -S wayprompt
     else
       fail "paru was not installed, ignore"
     fi
   fi
 
-  configure_gpg_agent_file $(which wayprompt)
+  configure_gpg_agent_file $pinentry_path
 }
 
 configure_gpg_agent_file () {
@@ -69,8 +72,10 @@ configure_gpg_agent_file () {
   cat "$DIRPATH/gpg-agent.example.conf" | sed 's|^# pinentry-program|pinentry-program '"$pinentry_path"'|' > "${DIRPATH}/${gnupg_name}"
   link_file "${DIRPATH}/${gnupg_name}" "$gnupg_dst/${gnupg_name}"
 
-  echo "Enable gpg store passphrase in keychain"
-  defaults write org.gpgtools.common DisableKeychain -bool no
+  if [ os_name == "macos" ]; then
+    echo "Enable gpg store passphrase in keychain"
+    defaults write org.gpgtools.common DisableKeychain -bool no
+  fi
 
   gpgconf --kill gpg-agent
   success "Success gpg-agent"
@@ -105,7 +110,7 @@ configure_linux_pinentry () {
   local path=""
   user "Which pinentry binary do you want to use?\n\
         1 pinentry-tty\n\
-        2 pinentry-cursues\n\
+        2 pinentry-curses\n\
         3 wayprompt\n\
         4 back to top menu\n\
         5 skip this step"
@@ -115,7 +120,7 @@ configure_linux_pinentry () {
     1)
       install_pinentry_tty;;
     2)
-      install_pinentry_cursues;;
+      install_pinentry_curses;;
     3)
       install_wayprompt;;
     4)
@@ -158,7 +163,7 @@ choose_pinentry_binary () {
   if [ "$skip" == "true" ]; then
     success "skipped"
   else
-    case "$(uname)" in
+    case $(os_name) in
       macos)
         configure_macos_pinentry;;
       linux)
