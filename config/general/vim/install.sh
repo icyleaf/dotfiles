156e4939fb6079cc58219a2bd265e46db73487bb
icyleaf <icyleaf.cn@gmail.com>
2025-04-01 17:02:14 +0800

    refactor(general): use neovim and lazyvim

--- config/general/vim/install.sh
+++ config/general/vim/install.sh
@@ -5,14 +5,10 @@ source functions/_lib.sh
 
 if test $(command -v nvim); then
   nvim_path="${HOME:-~}/.config/nvim"
-  if ! [ -d "$spacevim_path" ]; then
+  if ! [ -d "$nvim_path" ]; then
     git clone https://github.com/LazyVim/starter $nvim_path
   fi
   success "LazyVim"
-elif test $(command -v vim); then
-  spacevim_path="${HOME:-~}/.SpaceVim"
-  if ! [ -d "$spacevim_path" ]; then
-    curl -sLf https://spacevim.org/install.sh | bash
-  fi
-  success "SpaceVim"
-end
+else
+  echo "Install neovim please"
+fi
