bdc3e2c9e4a17738666292ca54be02c9c21b2a23
icyleaf <icyleaf.cn@gmail.com>
2025-09-12 14:25:42 +0800

    feat: add linux kitty

---
+++ config/linux/kitty/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# 链接 kitty 的配置文件
+
+source functions/_lib.sh
+
+dest_path="${HOME:-~}/.config/kitty"
+
+mkdir -p $dest_path 2> /dev/null
+link_file "${DIRPATH}/kitty.conf" "${dest_path}/kitty.conf"
+link_file "${DIRPATH}/hyde.conf" "${dest_path}/hyde.conf"
+link_file "${DIRPATH}/theme.conf" "${dest_path}/theme.conf"
