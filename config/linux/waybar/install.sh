227d2ffd8bd725bfb4212fe71c437b0dc481f0f0
icyleaf <icyleaf.cn@gmail.com>
2025-09-17 09:10:59 +0800

    refactor(linux): remove unused waybar theme file

--- config/linux/waybar/install.sh
+++ config/linux/waybar/install.sh
@@ -9,4 +9,3 @@ dest_path="${HOME:-~}/.config/waybar"
 mkdir -p $dest_path 2> /dev/null
 link_file "${DIRPATH}/config.jsonc" "${dest_path}/config.jsonc"
 link_file "${DIRPATH}/style.css" "${dest_path}/style.css"
-link_file "${DIRPATH}/theme.css" "${dest_path}/theme.css"
