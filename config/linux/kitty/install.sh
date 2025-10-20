eb2c1a38bb671d940eee940c8fbc1fa43fe973fb
icyleaf <icyleaf.cn@gmail.com>
2025-10-20 09:36:59 +0800

    linux(kitty): update kitty configure

--- config/linux/kitty/install.sh
+++ config/linux/kitty/install.sh
@@ -10,3 +10,4 @@ mkdir -p $dest_path 2> /dev/null
 link_file "${DIRPATH}/kitty.conf" "${dest_path}/kitty.conf"
 link_file "${DIRPATH}/general.conf" "${dest_path}/general.conf"
 link_file "${DIRPATH}/theme.conf" "${dest_path}/theme.conf"
+link_file "${DIRPATH}/keybindings.conf" "${dest_path}/keybindings.conf"
