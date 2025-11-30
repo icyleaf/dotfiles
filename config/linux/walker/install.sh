ad35ebb36daea8e852b0117cdfa8d588a9f7aa2d
icyleaf <icyleaf.cn@gmail.com>
2025-11-30 10:09:43 +0800

    linux(walker): merge config

--- config/linux/walker/install.sh
+++ config/linux/walker/install.sh
@@ -7,5 +7,5 @@ source functions/_lib.sh
 dest_path="${HOME:-~}/.config/walker"
 
 mkdir -p $dest_path 2> /dev/null
-link_file "${DIRPATH}/config.json" "${dest_path}/config.json"
+link_file "${DIRPATH}/config.toml" "${dest_path}/config.toml"
 link_file "${DIRPATH}/themes" "${dest_path}/themes"
