6c80543cf4b5532d25e01df7fbca36ce8be27efa
icyleaf <icyleaf.cn@gmail.com>
2026-01-06 10:48:44 +0800

    refactor(general): remove sync settings extention for vscode

--- config/general/vscode/install.sh
+++ config/general/vscode/install.sh
@@ -13,19 +13,4 @@ if test ! $(which code); then
   link_file $code_path /usr/local/bin/code
 fi
 
-settings_sync_file="$HOME/Library/Application Support/Code/User/syncLocalSettings.json"
-
-if [ -z "$(code --list-extensions| grep Shan.code-settings-sync)" ]; then
-  info "Install Settings Sync"
-  code --install-extension Shan.code-settings-sync
-fi
-
-if ! [ -f "vscode/syncLocalSettings.json" ]; then
-  user ' - What is your github access token?'
-  read -e github_access_token
-
-  sed -e "s/GITHUB_ACCESS_TOKEN/$github_access_token/g" vscode/syncLocalSettings.json.example > vscode/syncLocalSettings.json
-fi
-
-link_file "${DIRPATH}/syncLocalSettings.json" "$settings_sync_file"
 success "Setup VSCode"
