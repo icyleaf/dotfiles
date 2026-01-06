22e26e1a7273070a9d9ee1ea14714f67898031df
icyleaf <icyleaf.cn@gmail.com>
2026-01-06 10:49:08 +0800

    refactor(general): remove install ruby version, use mise config instead

--- config/general/ruby/install.sh
+++ config/general/ruby/install.sh
@@ -3,17 +3,9 @@
 # Ruby 依赖文件
 source functions/_lib.sh
 
-ruby_version=3.3.5
-
 if ! test $(which mise); then
   fail "abort, mise not installed"
 fi
-
-if [ -z "$(ruby -v | grep $ruby_version)" ]; then
-  info "Install Ruby $ruby_version"
-  mise install ruby $ruby_version
-fi
-mise global ruby $ruby_version &> /dev/null
 
 for gem in "irbtools" \
   "awesome_print" \
