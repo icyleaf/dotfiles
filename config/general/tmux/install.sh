5060730600b27dec0b251f6889cc33006745dd28
icyleaf <icyleaf.cn@gmail.com>
2026-07-06 23:15:57 +0900

    refactor(generate): reduce tmux config

--- config/general/tmux/install.sh
+++ config/general/tmux/install.sh
@@ -10,31 +10,31 @@ tpm_url="https://github.com/tmux-plugins/tpm"
 # 检测 tmux 版本，决定配置文件位置
 # tmux 3.2+ 支持 XDG Base Directory 规范 (~/.config/tmux/tmux.conf)
 # 旧版本只支持 ~/.tmux.conf
-get_tmux_version() {
-  tmux -V 2>/dev/null | sed -E 's/.*[^0-9]([0-9]+\.[0-9]+).*/\1/'
-}
+# get_tmux_version() {
+#   tmux -V 2>/dev/null | sed -E 's/.*[^0-9]([0-9]+\.[0-9]+).*/\1/'
+# }
 
-version_gte() {
-  printf '%s\n%s\n' "$1" "$2" | sort -V -C 2>/dev/null
-}
+# version_gte() {
+#   printf '%s\n%s\n' "$1" "$2" | sort -V -C 2>/dev/null
+# }
 
-TMUX_VERSION=$(get_tmux_version)
+# TMUX_VERSION=$(get_tmux_version)
 
-if version_gte "$TMUX_VERSION" "3.2"; then
-  dest_path="${HOME:-~}/.config/tmux/"
-  tpm_path="${dest_path}plugins/tpm"
-  mkdir -p "$dest_path/plugins"
-  info "Detected tmux $TMUX_VERSION (>= 3.2), using XDG config: ~/.config/tmux/"
-else
+# if version_gte "$TMUX_VERSION" "3.2"; then
+#   dest_path="${HOME:-~}/.config/tmux/"
+#   tpm_path="${dest_path}plugins/tpm"
+#   mkdir -p "$dest_path/plugins"
+#   info "Detected tmux $TMUX_VERSION (>= 3.2), using XDG config: ~/.config/tmux/"
+# else
   dest_path="${HOME:-~}/.tmux/"
   tpm_path="${dest_path}plugins/tpm"
   mkdir -p "$dest_path/plugins"
   # 旧版本需要创建 ~/.tmux.conf 软链接
   link_file "${DIRPATH}/${source_path}" "${HOME:-~}/.tmux.conf"
-  info "Detected tmux $TMUX_VERSION (< 3.2), using legacy config: ~/.tmux.conf"
-fi
+  # info "Detected tmux $TMUX_VERSION (< 3.2), using legacy config: ~/.tmux.conf"
+# fi
 
-link_file "${DIRPATH}/${source_path}" "$dest_path/$source_path"
+# link_file "${DIRPATH}/${source_path}" "$dest_path/$source_path"
 
 info "Installing tpm"
 if ! [ -d "$tpm_path" ]; then
