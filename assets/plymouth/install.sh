725d1b3a5b8fbf4a159ccb87f4d787725d662e7c
icyleaf <icyleaf.cn@gmail.com>
2026-07-26 14:36:33 +0900

    feat: add gemrc and editorconfig

--- assets/plymouth/install.sh
+++ assets/plymouth/install.sh
@@ -2,8 +2,6 @@
 #
 # 安装 plymouth 主题
 
-source functions/_lib.sh
-
 # get hostname
 hostname=$(hostname)
 
@@ -16,16 +14,15 @@ if [[ ! -d "$src_theme_path" ]]; then
   theme_name="achron"
 fi
 
-
 echo "Installing plymouth theme ${theme_name}...${src_theme_path}"
 
 if [[ -d "$src_theme_path" ]]; then
-  info "Installing plymouth theme ... ${theme_name}"
+  echo "Installing plymouth theme ... ${theme_name}"
   sudo mkdir -p "/usr/share/plymouth/themes"
   sudo cp -rf "$src_theme_path" "/usr/share/plymouth/themes/"
 
-  info "Setting plymouth theme ${theme_name} as default..."
+  echo "Setting plymouth theme ${theme_name} as default..."
   sudo plymouth-set-default-theme "$theme_name"
 
-  success "plymouth theme ${theme_name} installed and set as default successfully."
+  echo "plymouth theme ${theme_name} installed and set as default successfully."
 fi
