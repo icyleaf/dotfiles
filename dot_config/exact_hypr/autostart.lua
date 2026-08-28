0f93de641d31f6b6851124af02a33c06796d38df
icyleaf <icyleaf.cn@gmail.com>
2026-08-29 00:58:37 +0900

    refactor(linux): remove un-used auto start cli for hyprland

--- dot_config/exact_hypr/autostart.lua
+++ dot_config/exact_hypr/autostart.lua
@@ -2,13 +2,10 @@
 
 hl.on("hyprland.start", function()
   -- User specific autostart applications
-  -- hl.exec_cmd(o.launch("hypridle"))                              -- Idle management
-  -- hl.exec_cmd(o.launch("waybar"))                                -- Status bar
   hl.exec_cmd(o.launch("wl-gammarelay-rs run"))                  -- Per-monitor software brightness
   hl.exec_cmd(o.launch("hypr-input-switcher -w"))                -- Input switch notification
   hl.exec_cmd(o.launch("synology-drive"))                        -- Synology Drive
   hl.exec_cmd(o.launch("keyd-application-mapper"))               -- Start keyd for app remapping
-  hl.exec_cmd(o.launch("swaync"))                                -- Notification panel daemon
   hl.exec_cmd(o.launch("udiskie --no-automount --smart-tray"))   -- Automount drives with tray icon
 
   -- Clipboard history (better than walker built-in clipboard module)
